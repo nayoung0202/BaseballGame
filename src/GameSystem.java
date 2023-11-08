39d41093c1de7f5c7a2fb4e5d1c3689d749ab6fe
s12171934 <ssong99999@naver.com>
2023-11-08 15:37:43 +0900

    create GameSystem class

---
+++ src/GameSystem.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameSystem {
+    public String createRandomNumber(int n){
+        String num = "";
+        List<Integer> arr = new ArrayList<>(List.of(0,1,2,3,4,5,6,7,8,9));
+        while(n > 0){
+            int random = (int)(Math.random() * arr.size());
+            num += String.valueOf(arr.get(random));
+            arr.remove(random);
+            n--;
+        }
+        return num;
+    }
+    public int strike(String target,String guess){
+        int count = 0;
+        int n = target.length();
+        for(int i = 0; i < n; i ++){
+            if(target.charAt(i) == guess.charAt(i)){
+                count += 1;
+            }
+        }
+        return count;
+    }
+    public int ball(String target,String guess){
+        int count = 0;
+        int n = target.length();
+        for(int i = 0; i < n; i ++){
+            if(guess.contains(String.valueOf(target.charAt(i)))){
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -1,45 +1,29 @@
+import jdk.dynalink.beans.StaticClass;
+
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
+    static ServerSocket serverSocket = null;
+    static ArrayList<Socket> socketList = new ArrayList<>();
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket();
+            serverSocket = new ServerSocket(50001);
+            while(socketList.size() <= 2){
+                System.out.println("대기중...");
+                Socket socket = serverSocket.accept();
+
+                socketList.add(socket);
+
+                new Thread(() -> {
+
+                }).start();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-    public String createRandomNumber(int n){
-        String num = "";
-        List<Integer> arr = new ArrayList<>(List.of(0,1,2,3,4,5,6,7,8,9));
-        while(n > 0){
-            int random = (int)(Math.random() * arr.size());
-            num += String.valueOf(arr.get(random));
-            arr.remove(random);
-            n--;
-        }
-        return num;
-    }
-    public int strike(String target,String guess){
-        int count = 0;
-        int n = target.length();
-        for(int i = 0; i < n; i ++){
-            if(target.charAt(i) == guess.charAt(i)){
-                count += 1;
-            }
-        }
-        return count;
-    }
-    public int ball(String target,String guess){
-        int count = 0;
-        int n = target.length();
-        for(int i = 0; i < n; i ++){
-            if(guess.contains(String.valueOf(target.charAt(i)))){
-                count += 1;
-            }
-        }
-        return count;
     }
 }
